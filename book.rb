31b634dd2bd638bbe22e0805b435a91df76b9ad8
Errytagedesign <errytage@gmail.com>
2023-10-17 10:08:05 +0100

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,5 +1,16 @@
+relative_require '/item.rb'
 class Book
-  def initialize
+    attr_accessor :publisher, :cover_state
 
-  end
+    def initialize(publisher, cover_state, publish_date, archived: false)
+      super(publish_date, archived: archived)
+      @publisher = publisher
+      @cover_state = cover_state
+    end
+
+    private
+
+    def can_be_archived?
+      super == true || @cover_state.downcase == 'bad'
+    end
 end
