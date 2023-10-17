d3ced71cacc48c307e181c03dd375828304d8762
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-17 13:39:38 +0300

    Add game class, game-list

--- item.rb
+++ item.rb
@@ -1,13 +1,13 @@
 class Item
   attr_accessor :id, :publish_date, :archived
-  attr_reader :genre
-  attr_writer :author, :label, :source
 
   def initialize(publish_date, archived, id = Random.rand(1..1000))
     @id = id
     @publish_date = publish_date
     @archived = archived
   end
+
+  attr_writer :genre, :author, :label, :source
 
   def can_be_archived?
     time = Time.now
@@ -17,16 +17,13 @@ class Item
     results > 10
   end
 
-  def genre=(genre)
-    @genre = genre
-    genre.items << self unless genre.items.include?(self)
-  end
-
   def move_to_archive
     @archived = true if can_be_archived? == true
   end
+
+  private :id, :can_be_archived, :archived
 end
 
-object = Item.new(Time.new(2015, 1, 1), false)
-puts object.can_be_archived?
-puts object.move_to_archive
+# object = Item.new(Time.new(2015, 1, 1), false)
+# puts object.can_be_archived?
+# puts object.move_to_archive
--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
+require_relative './app'
+
 def main_menu
-  puts 'Main Menu'
+  puts 'Please choose an option by enterin a number:'
   puts '1) List all Books'
   puts '2) List all Music Albums'
   puts '3) List all Games'
@@ -15,44 +17,42 @@ def main_menu
   puts '13) Exit the program'
 end
 
-def list_all_books
-  # Implement the logic to list all books
-  puts 'Listing all books...'
-end
-
-def list_all_music_albums
-  # Implement the logic to list all music albums
-  puts 'Listing all music albums...'
-end
-
-# Define methods for other menu options
-
-def exit_program
-  # Implement any cleanup or exit logic here
-  puts 'Exiting the program.'
+def options(app)
+  input = gets.chomp.to_i
+  case input
+  when 1 then app.books.list_all_books
+  when 2 then app.musics.list_all_music_albums
+  when 3 then app.games.list_all_games
+  when 4 then app.movies_sources.list_all_movies
+  when 5 then app.genres.list_all_genre
+  when 6 then app.labels.list_all_labels
+  when 7 then app.authors.list_all_authors
+  when 8 then app.movies_sources.list_all_sources
+  when 9 then app.books.add_book
+  when 10 then app.musics.add_music_album
+  when 11 then app.games.add_game_menu
+  when 12 then app.movies_sources.add_movie
+  when 13 then 13
+  else
+    puts 'Invalid number, please try again!'
+  end
 end
 
 def main
   running = true
 
+  app = App.new
+
+  app.obtain_data
+  puts 'Welcome to our App! 🎉'
   while running
     main_menu
-    input = gets.to_i
-
-    case input
-    when 1
-      list_all_books
-    when 2
-      list_all_music_albums
-    when 3
-      # Handle other menu options in a similar way
-    when 13
-      exit_program
-      running = false
-    else
-      puts 'Invalid option. Please select a valid option.'
-    end
+    option = options(app)
+    break if option == 13
   end
+  app.save_data
+
+  puts 'Thanks for using this App!! ✨'
 end
 
 main
