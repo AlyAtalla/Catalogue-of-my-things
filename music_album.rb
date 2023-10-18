6096c6a36e58f0418ced9f3ed199aa583179d289
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-18 11:21:09 +0300

    Refactor music album

--- music_album.rb
+++ music_album.rb
@@ -2,7 +2,6 @@ require_relative 'item'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify
-  attr_reader :id
 
   def initialize(on_spotify, publish_date, archived, id = Random.rand(1..1000))
     super(publish_date, archived, id)
